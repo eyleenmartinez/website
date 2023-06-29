088b55ecb3219829abf37de9931aea4c949366ad
eyleenmartinez <138134355+eyleenmartinez@users.noreply.github.com>
2023-06-29 09:11:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var firstname = "Eyleen";
+var lastname = "Martinez";
+var numberOfSiblings = 3;
+var hasPets = true
+
+console.log(firstname);
+console.log(lastname);
+console.log(numberOfSiblings);
+
+
+if(hasPets === true){
+    console.log("they are playfull");
+}
+
+var number = 0;
+while (number < 15){
+    console.log('8th grade is cool igzz');
+    number++;
+
+}
+
+
+
+var button = document.getElementById("hi");
+var body = document.body;
+var clickCount = 0
+
+
+body.setAttribute("style","background-color: black");
+var blood = document.getElementById("me");
+button,addEventListener("click", function(){
+   h1.innerHTML = " I'm tired ";
+  click ("eyleen")=back ("ground-color-pink")
+
+});
